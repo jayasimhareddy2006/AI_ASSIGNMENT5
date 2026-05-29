8e1d8d4bea16657e013fb777bb55547d5ceaebcd
Jayasimha Reddy Anreddy <jayasimhareddy2006@gmail.com>
2026-05-29 15:42:56 +0530

    Bayesian Networks.py

--- Bayesian Networks/Bayesian Networks.py
+++ Bayesian Networks/Bayesian Networks.py
@@ -1,30 +1,98 @@
 # ==========================================
-# BAYESIAN NETWORK EXAMPLE
+# MEDICAL DIAGNOSIS USING BAYESIAN NETWORK
 # ==========================================
-print("\n===================================")
-print("BAYESIAN NETWORK")
-print("===================================")
-P_rain = 0.30
-P_wet_given_rain = 0.90
-P_wet_given_no_rain = 0.10
 
-P_wet = (
-    P_wet_given_rain * P_rain
-    +
-    P_wet_given_no_rain * (1 - P_rain)
+print("\n===================================")
+print("MEDICAL DIAGNOSIS BAYESIAN NETWORK")
+print("===================================")
+
+print("\nNetwork Structure:")
+print("Disease ---> Fever")
+print("Disease ---> Cough")
+
+# Prior Probability
+
+P_disease = 0.10
+
+# Conditional Probabilities
+
+P_fever_given_disease = 0.80
+P_fever_given_no_disease = 0.20
+
+P_cough_given_disease = 0.70
+P_cough_given_no_disease = 0.30
+
+print("\nPrior Probability:")
+print("P(Disease) =", P_disease)
+
+print("\nConditional Probabilities:")
+print(
+    "P(Fever | Disease) =",
+    P_fever_given_disease
 )
-P_rain_given_wet = (
-    P_wet_given_rain * P_rain
-) / P_wet
-print("\nExample:")
-print("Rain -> Wet Grass")
-print("\nP(Rain) =", P_rain)
-print("P(WetGrass | Rain) =", P_wet_given_rain)
-print("P(WetGrass | No Rain) =", P_wet_given_no_rain)
+
+print(
+    "P(Fever | No Disease) =",
+    P_fever_given_no_disease
+)
+
+print(
+    "P(Cough | Disease) =",
+    P_cough_given_disease
+)
+
+print(
+    "P(Cough | No Disease) =",
+    P_cough_given_no_disease
+)
+
+# Evidence
+
+print("\nEvidence:")
+print("Patient has Fever")
+
+# Bayes Theorem
+
+P_fever = (
+    P_fever_given_disease * P_disease
+    +
+    P_fever_given_no_disease
+    * (1 - P_disease)
+)
+
+P_disease_given_fever = (
+    P_fever_given_disease
+    * P_disease
+) / P_fever
 
 print("\nInference Result:")
+
 print(
-    "P(Rain | Wet Grass) =",
-    round(P_rain_given_wet, 4)
+    "P(Disease | Fever) =",
+    round(
+        P_disease_given_fever,
+        4
+    )
 )
+
+if P_disease_given_fever > 0.5:
+
+    print(
+        "\nConclusion:"
+    )
+
+    print(
+        "Disease is likely."
+    )
+
+else:
+
+    print(
+        "\nConclusion:"
+    )
+
+    print(
+        "Disease is unlikely."
+    )
+
 print("\n===================================")
